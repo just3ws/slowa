21cf6c18251f9189da53afa2931fc601fd38707c
Mike Hall <mike@just3ws.com>
2018-08-22 07:16:25 -0500

    Updated annotations

--- app/serializers/letter_jumble_serializer.rb
+++ app/serializers/letter_jumble_serializer.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: letter_jumbles
+#
+#  id         :bigint(8)        not null, primary key
+#  letters    :string           not null
+#  words      :string           default([]), is an Array
+#  version    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class LetterJumbleSerializer
   include FastJsonapi::ObjectSerializer
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 
 # == Route Map
 #
-#         Prefix Verb URI Pattern                   Controller#Action
-# letter_jumbles GET  /letter_jumbles(.:format)     letter_jumbles#index
-#                POST /letter_jumbles(.:format)     letter_jumbles#create
-#  letter_jumble GET  /letter_jumbles/:id(.:format) letter_jumbles#show
+#         Prefix Verb URI Pattern                        Controller#Action
+# letter_jumbles GET  /letter_jumbles(.:format)          letter_jumbles#index {:format=>"json"}
+#                POST /letter_jumbles(.:format)          letter_jumbles#create {:format=>"json"}
+#                GET  /letter_jumbles/:letters(.:format) letter_jumbles#show {:format=>"json"}
 
 Rails.application.routes.draw do
   resources(:letter_jumbles, only: %i[create index], constraints: { format: 'json' })
